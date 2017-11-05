c2b16682c171665450ff72c502500632821798a0
Lachezar <ninkov82@gmail.com>
2017-11-05 09:20:27 +0200

    methods lab

---
+++ C#Lab Methods, Debugging and Troubleshooting Code/01. Blank Receipt.cs
@@ -0,0 +1,40 @@
+
+namespace BlankReceipt
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class BlankReceipt
+    {
+        static void Main()
+        {
+            PrintReceipt();
+        }
+
+        public static void PrintReceipt()
+        {
+            PrintHeader();
+            PrintBody();
+            PrintFooter();
+
+        }
+        public static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+        public static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        public static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+
+    }
+
+}
+
