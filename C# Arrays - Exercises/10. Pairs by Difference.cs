c806c63259033f7709affa5f91e5243600a1ca6d
Ninkov <ninkov82@gmail.com>
2018-02-21 21:08:22 +0200

    Update and rename 10. IndexOfLetters.cs to 10. Pairs by Difference.cs

---
+++ C# Arrays - Exercises/10. Pairs by Difference.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairsByDifference
+{
+    class PairsByDifference
+    {
+        static void Main()
+        {
+		int[] numbers = Console.ReadLine()
+			.Split
+			(new[]{' '},StringSplitOptions.RemoveEmptyEntries)
+			.Select(int.Parse)
+			.ToArray();
+		
+		int differences = int.Parse(Console.ReadLine());
+									
+		var counter = 0;
+		
+		for(int i = 0;i < numbers.Length; i++)
+		{
+			//int currentNum = numbers[i];
+			for(int j = i;j<numbers.Length;j++)
+			{
+				if(Math.Abs(numbers[i]-numbers[j])==differences)
+				{
+					counter++;
+				}
+			}
+		}
+			Console.WriteLine(counter);
+	}
+  }
+}
