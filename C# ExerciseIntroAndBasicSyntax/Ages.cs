f3f34d432f506825c32883ab4d2592c252d990be
Ninkov <ninkov82@gmail.com>
2019-10-08 21:33:56 +0300

    new task

---
+++ C# ExerciseIntroAndBasicSyntax/Ages.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace NewTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (input>= 66)
+            {
+                Console.WriteLine("elder");
+            }
+            else if (input>=0 && input<=2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (input >= 3 && input <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (input >= 14 && input <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (input >= 20 && input <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+        }
+    }
+}
