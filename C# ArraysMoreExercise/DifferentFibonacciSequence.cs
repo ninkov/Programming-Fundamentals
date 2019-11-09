8034bd708395b0edd6ee960c1757abcaf6532d7d
Ninkov <ninkov82@gmail.com>
2019-11-09 14:16:17 +0200

    part of  home work

---
+++ C# ArraysMoreExercise/DifferentFibonacciSequence.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main()
+        {
+            //Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            long result = Fib(n);
+            //Console.WriteLine("fib({0}) = {1}", n, result);
+
+            Console.WriteLine(result);
+        }
+        static long Fib(int n)
+        {
+            long totalFiboNumber = 1;
+            long firstFiboNumber = 1;
+            long secondFiboNumber = 1;
+            for (int i = 2; i < n; i++)
+            {
+                totalFiboNumber = firstFiboNumber + secondFiboNumber;
+                secondFiboNumber = firstFiboNumber;
+                firstFiboNumber = totalFiboNumber;
+            }
+            return totalFiboNumber;
+        }
+    }
+}
+
---
+++ C# ArraysMoreExercise/FibonacciSequence.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main()
+        {
+
+            int input = int.Parse(Console.ReadLine());
+            int first = 0;
+            int second = 1;
+            int third = 0;
+            //Console.Write("First {0} Fibonacci numbers {1} {2} ", input, first, second);
+
+            for (int i = 3; i <= input; i++)
+            {
+                third = first + second;
+              //  Console.Write("{0} ", third);
+                first = second;
+                second = third;
+
+            }
+            int sum = first + second;
+            Console.WriteLine(sum);
+        }
+    }
+}
+
